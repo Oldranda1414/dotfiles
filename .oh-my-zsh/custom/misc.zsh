be44390bf8b5f90914df2dbaf1905299a2af8268
Oldranda <leonardorandacio@gmail.com>
2025-06-20 13:25:21 +0200

    remove commented code

--- .oh-my-zsh/custom/misc.zsh
+++ .oh-my-zsh/custom/misc.zsh
@@ -17,27 +17,6 @@
   # Bind <ctrl>-f to accept autosuggest as with emacs bindkey
   bindkey -M viins '^F' autosuggest-accept
 
-  # Start in vim normal mode by default
-  # Commented out couse I'm still deciding if I prefer insert or normal on default
-  # autoload -Uz edit-command-line
-  # zle-line-init() { zle vi-cmd-mode; }
-
-  # Thin cursor when in insert mode
-  # THIS SADLY DOES NOT WORK IN MY CURRENT TERMINAL
-  # function zle-keymap-select {
-  #   if [[ $KEYMAP == vicmd ]]; then
-  #     echo -ne '\e[2 q'  # Fat block for NORMAL
-  #   elif [[ $KEYMAP == viins ]]; then
-  #     echo -ne '\e[6 q'  # Thin bar for INSERT
-  #   fi
-  #   zle reset-prompt
-  # }
-
-  # Hook functions
-  # zle -N zle-line-init
-  # zle -N zle-keymap-select
-# }
-
 # Setting fzf theme to catpuccin-mocha (https://github.com/catppuccin/fzf) 
 export FZF_DEFAULT_OPTS=" \
 --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
