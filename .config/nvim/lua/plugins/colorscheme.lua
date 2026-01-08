023fe963a39903ecb90eae44060b29cd82a970fc
Oldranda <leonardorandacio@gmail.com>
2026-01-08 12:44:15 +0100

    improve nvim colorscheme config, update nvim config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,17 @@
--- taken from https://github.com/oskarnurm/koda.nvim
--- return {
---   "oskarnurm/koda.nvim",
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other start plugins
---   config = function()
---     -- require("koda").setup({ transparent = true })
---     vim.cmd("colorscheme koda")
---   end,
--- }
-
 return {
-  "blazkowolf/gruber-darker.nvim",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  opts = {
-    italic = {
-      strings = false,
-      comments = false,
+  {
+    "blazkowolf/gruber-darker.nvim",
+    opts = {
+      italic = {
+        strings = false,
+        comments = false,
+      },
     },
   },
-  config = function(_, opts)
-    require("gruber-darker").setup(opts)
-    vim.cmd("colorscheme gruber-darker")
-  end,
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruber-darker",
+    },
+  },
 }
