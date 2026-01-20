a0f62121ec5c42189818760269d76ce18c35ce6b
Oldranda <leonardorandacio@gmail.com>
2026-01-20 16:24:39 +0100

    update gitignore, solve nvim config error

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Set spell-checker for .tex files to be it.
+-- This is done to work on my master thesis, which is in italian.
+-- Once the thesis is finished, this could probably be removed.
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "tex",
+  callback = function()
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = { "it" }
+  end,
+})
